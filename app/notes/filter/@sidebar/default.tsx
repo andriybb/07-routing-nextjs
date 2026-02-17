7626807a36b3c6548d46d2532ca762c6868570f0
andriy <and123bar@gmail.com>
2026-02-17 22:49:02 +0200

    fix bugs3

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,41 +1,19 @@
 import Link from 'next/link';
 import css from './SidebarNotes.module.css';
+import type { NoteTag } from '@/types/note';
+
+const tags: (NoteTag | 'all')[] = ['all', 'Todo', 'Personal', 'Work', 'Meeting', 'Shopping'];
 
 export default function SidebarNotes() {
   return (
     <ul className={css.menuList}>
-      <li className={css.menuItem}>
-        <Link href={`/notes/filter/all`} className={css.menuLink}>
-          <strong>All notes</strong>
-        </Link>
-      </li>
-      <li className={css.menuItem}>
-        <Link href={`/notes/filter/Todo`} className={css.menuLink}>
-          <strong>Todo</strong>
-        </Link>
-      </li>
-      <li className={css.menuItem}>
-        <Link href={`/notes/filter/Personal`} className={css.menuLink}>
-          <strong>Personal</strong> 
-        </Link>
-      </li>
-      <li className={css.menuItem}>
-        <Link href={`/notes/filter/Work`} className={css.menuLink}>
-         <strong>Work</strong>
-        </Link>
-      </li>
-      <li className={css.menuItem}>
-  <Link href={`/notes/filter/Meeting`} className={css.menuLink}>
-    <strong>Meeting</strong>
-  </Link>
-</li>
-      <li className={css.menuItem}>
-        <Link href={`/notes/filter/Shopping`} className={css.menuLink}>
-         <strong>Shopping</strong>
-        </Link>
-      </li>
+      {tags.map((tag) => (
+        <li key={tag} className={css.menuItem}>
+          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+            <strong>{tag === 'all' ? 'All notes' : tag}</strong>
+          </Link>
+        </li>
+      ))}
     </ul>
-    
-
   );
 }
